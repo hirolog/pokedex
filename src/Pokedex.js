54aed5ff1f4e74070ef15ab30923f2249c5276b0
hirolog <public.hiro@gmail.com>
2020-12-28 16:42:38 +0900

    Add Pokemon Cards

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -1,7 +1,47 @@
 import React from "react";
+import {
+  AppBar,
+  Card,
+  CardContent,
+  Grid,
+  makeStyles,
+  Toolbar
+} from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  pokedexContainer: {
+    paddingTop: "20px",
+    paddingLeft: "50px",
+    paddingRight: "50px"
+  }
+}));
+
+const getPokemonCard = () => {
+  return (
+    <Grid item xs={4}>
+      <Card>
+        <CardContent>This is a Pokemon Card.</CardContent>
+      </Card>
+    </Grid>
+  );
+};
 
 const Pokedex = () => {
-  return <div>Pokedex</div>;
+  const classes = useStyles();
+  return (
+    <React.StrictMode>
+      <AppBar position="static">
+        <Toolbar>Menu</Toolbar>
+      </AppBar>
+      <Grid container spacing={3} className={classes.pokedexContainer}>
+        {getPokemonCard()}
+        {getPokemonCard()}
+        {getPokemonCard()}
+        {getPokemonCard()}
+        {getPokemonCard()}
+      </Grid>
+    </React.StrictMode>
+  );
 };
 
 export default Pokedex;
